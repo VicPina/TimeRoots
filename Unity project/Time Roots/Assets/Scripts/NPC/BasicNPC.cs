1b80e7f22ba9ebf3d8b0557cc196da2c0a269c5e
Abby Martin <ac2marti@uwaterloo.ca>
2023-02-05 12:05:02 -0800

    adding dialogue

--- Unity project/Time Roots/Assets/Scripts/NPC/BasicNPC.cs
+++ Unity project/Time Roots/Assets/Scripts/NPC/BasicNPC.cs
@@ -19,51 +19,52 @@ public class BasicNPC : MonoBehaviour
     public string messageToShow;
     [Tooltip("Check if this character is going to be interactable multiple times")]
     public bool multipleDialogues;
-    [Tooltip("List of dialogues")]
-    public string[] dialogueMessages;
+   // [Tooltip("List of dialogues")]
+    //public string[] dialogueMessages;
     [HideInInspector]
     public int numConversations; //To indicate which set of dialog tp show - currently not being used but here if we have time. 
+
     private void Awake()
     {
         //click = GetComponentInChildren<Button>();
         numConversations = 0;
-        messageToShow = dialogueMessages[numConversations];
+       // messageToShow = dialogueMessages[numConversations];
     }
+
     public void CheckForMoreDialogues()
     {
-        if (multipleDialogues && numConversations<=dialogueMessages.Length)
-        {
-            numConversations++;
-            messageToShow = dialogueMessages[numConversations];
-        }
+        //if (multipleDialogues && numConversations<=dialogueMessages.Length)
+       // {
+          //  numConversations++;
+          //  messageToShow = dialogueMessages[numConversations];
+        //}
     }
-    /*
-     * 
-     * / MARK - Dialogue
+
     public GameObject DialoguePanel;
-    //public Message[] dialogueMessages; //The list of dialoge to be presented
+    [Tooltip("List of dialogues")]
+    public Message[] dialogueMessages; //The list of dialoge to be presented
     public Actor[] dialogueParticipants;
 
     // MARK - Interactable
-    private Button click;
+    //private Button click;
 
     //Check that an NPC cant be a parent of thier own: NPC rules
 
-    void Start()
-    {
-        click.onClick.AddListener(TaskOnClick);
-    }
+   // void Start()
+    //{
+       // click.onClick.AddListener(TaskOnClick);
+    //}
 
-    void TaskOnClick()
-    {
+   // void TaskOnClick()
+    //{
         //Open up the dialogue
         // TODO - can set the position above via: Instantiate(GameObject objectToSpawn, Vector3 position, Quaternion rotation);
-        GameObject dialoguePanel = Instantiate(DialoguePanel);
-        DialogueManager dialogueManager = dialoguePanel.GetComponent<DialogueManager>();
-        dialogueManager.OpenDialogue(dialogueMessages, dialogueParticipants);
+       // GameObject dialoguePanel = Instantiate(DialoguePanel);
+       // DialogueManager dialogueManager = dialoguePanel.GetComponent<DialogueManager>();
+        //dialogueManager.OpenDialogue(dialogueMessages, dialogueParticipants);
 
-        Debug.Log("NPC clicked");
-    }
+        //Debug.Log("NPC clicked");
+   // }//
 
     public void ShowDialogue()
     {
@@ -71,5 +72,4 @@ public class BasicNPC : MonoBehaviour
         DialogueManager dialogueManager = dialoguePanel.GetComponent<DialogueManager>();
         dialogueManager.OpenDialogue(dialogueMessages, dialogueParticipants);
     }
-    */
 }
