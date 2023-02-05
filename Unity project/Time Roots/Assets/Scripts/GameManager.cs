f7851ddb0b40a6182c5e8eaaf7ddeebcae3c4fcc
VicPina <47024323+VicPina@users.noreply.github.com>
2023-02-04 22:06:46 -0600

    Dialogue pull issue

--- Unity project/Time Roots/Assets/Scripts/GameManager.cs
+++ Unity project/Time Roots/Assets/Scripts/GameManager.cs
@@ -8,14 +8,15 @@ public class GameManager : MonoBehaviour
 {
     public Transform npcPanel;
 
-    public void OpenTextPanel(NPCInfoTest npcToRead)
+    public void OpenTextPanel(BasicNPC npcToRead)
     {
         npcPanel.gameObject.SetActive(true);
         foreach (Transform uiElement in npcPanel)
         {
-            if (uiElement.gameObject.tag == "NPC Name") { uiElement.GetComponent<TextMeshProUGUI>().text = npcToRead.characterName; }
-            if (uiElement.gameObject.tag == "NPC Avatar") { uiElement.GetComponent<Image>().sprite = npcToRead.uiAvatar; }
-            if (uiElement.gameObject.tag == "NPC Dialogue") { uiElement.GetComponent<TextMeshProUGUI>().text = npcToRead.dialogueMsg; }
+            if (uiElement.gameObject.tag == "NPC Name") { uiElement.GetComponent<TextMeshProUGUI>().text = npcToRead.NPCName; }
+            if (uiElement.gameObject.tag == "NPC Avatar") { uiElement.GetComponent<Image>().sprite = npcToRead.NPCSprite; }
+            if (uiElement.gameObject.tag == "NPC Dialogue") { uiElement.GetComponent<TextMeshProUGUI>().text = npcToRead.messageToShow; }
         }
+        //textMesh.text = message;
     }
 }
