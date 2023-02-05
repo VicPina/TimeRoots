c45b29ae67158653ac70f66c39a95e4a3222b4ee
Faisal Atif <faisal@Faisals-MacBook-Pro-2.local>
2023-02-05 11:19:06 -0800

    added the npc characters and gave them a basic route

--- Unity project/Time Roots/Assets/Scripts/NPC/NPCMovement.cs
+++ Unity project/Time Roots/Assets/Scripts/NPC/NPCMovement.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class NPCMovement : MonoBehaviour
 {
     public BasicNPC NPC;
+    public int speed = 2;
     public Transform target0;
     public Transform target1;
     public Transform target2;
     public Transform target3;
+    public Transform target4;
+    public Transform target5;
+    public Transform target6;
+    public Transform target7;
+    public Transform target8;
+    public Transform target9;
+    public Transform target10;
+    public Transform target11;
+    public Transform target12;
+    public Transform target13;
+    public Transform target14;
+
     Rigidbody2D rigidbody2D;
     Vector2 direction;
 
@@ -18,7 +31,7 @@ public class NPCMovement : MonoBehaviour
 
     // The places that the NPCs visit
     public Transform[] targets;
-    public int numberofTargets = 4;
+    public int numberofTargets = 15;
 
     // private moveDirection = new Vector2;
 
@@ -71,12 +84,45 @@ public class NPCMovement : MonoBehaviour
             case 3:
                 moveTo(target3);
                 break;
+            case 4:
+                moveTo(target4);
+                break;
+            case 5:
+                moveTo(target5);
+                break;
+            case 6:
+                moveTo(target6);
+                break;
+            case 7:
+                moveTo(target7);
+                break;
+            case 8:
+                moveTo(target8);
+                break;
+            case 9:
+                moveTo(target9);
+                break;
+            case 10:
+                moveTo(target10);
+                break;
+            case 11:
+                moveTo(target11);
+                break;
+            case 12:
+                moveTo(target12);
+                break;
+            case 13:
+                moveTo(target13);
+                break;
+            case 14:
+                moveTo(target14);
+                break;
         }
     }
 
     void moveTo(Transform target){
         direction = (target.position - transform.position).normalized;
-        rigidbody2D.velocity = direction * 3;
+        rigidbody2D.velocity = direction * speed;
         // Debug.Log(direction.x);
 
 
@@ -84,7 +130,11 @@ public class NPCMovement : MonoBehaviour
         {
             //status is used as an indicator to show which place from an ordered list of places to go to next
             //The list should be unique to every NPC
-            status = status+1;
+            if (status<14){
+                status = status+1;
+            } else{
+                status =0;
+            }
         }
     }
 
